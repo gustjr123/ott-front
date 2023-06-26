0d581453d9e6777c6a35938422c369deb6651468
hyeonseok <hs.cho9647@gmail.com>
2023-06-26 16:23:35 +0900

    cookie 추가 및 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ApiComponent from "./component/api";
-import VideoComponent from "./component/video";
-import TokenComponent from "./component/token";
+// import VideoComponent from "./component/video";
+// import TokenComponent from "./component/token";
+import CookieComponent from "./component/cookie";
 
 const App = () => {
   return (
@@ -9,11 +10,14 @@ const App = () => {
       <h1>영상 List 요청</h1>
       <ApiComponent />
 
-      <h1>Token 및 영상링크 요청</h1>
-      <TokenComponent />
+      {/* <h1>Token 및 영상링크 요청</h1>
+      <TokenComponent /> */}
 
-      <h1>동영상 재생</h1>
-      <VideoComponent />
+      <h1>Cookie 생성 및 링크 새창</h1>
+      <CookieComponent />
+
+      {/* <h1>동영상 재생</h1>
+      <VideoComponent /> */}
     </div>
   );
 };
