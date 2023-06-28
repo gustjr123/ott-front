d0fc1fe3570329d3826e537d181a1102ea4be088
hyeonseok <hs.cho9647@gmail.com>
2023-06-28 15:31:34 +0900

    css 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import ApiComponent from "./component/api";
-// import VideoComponent from "./component/video";
-// import TokenComponent from "./component/token";
 import CookieComponent from "./component/cookie";
+import logo from "./assets/images/logo.png";
+import "./styles/App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>영상 List 요청</h1>
-      <ApiComponent />
+    <div className="app-container">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo-image" />
+      </div>
+      <div className="content-container">
+        <h1 className="title">영상 List</h1>
+        <ApiComponent />
 
-      {/* <h1>Token 및 영상링크 요청</h1>
-      <TokenComponent /> */}
-
-      <h1>Cookie 생성 및 링크 새창</h1>
-      <CookieComponent />
-
-      {/* <h1>동영상 재생</h1>
-      <VideoComponent /> */}
+        <h1 className="title">Cookie 생성 및 링크 새창</h1>
+        <CookieComponent />
+      </div>
     </div>
   );
 };
