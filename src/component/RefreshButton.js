89a26be69c289942ee7af1d5ad5f7e5b7c5b2274
hyeonseok <hs.cho9647@gmail.com>
2023-07-10 11:59:49 +0900

    edit: 새로고침 추가 및 기존 입력창 제거

---
+++ src/component/RefreshButton.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { FiRefreshCw } from "react-icons/fi";
+import "../styles/RefreshButton.css";
+
+const RefreshButton = ({ onRefresh }) => {
+  const [rotating, setRotating] = useState(false);
+
+  const handleRefresh = () => {
+    if (!rotating) {
+      setRotating(true);
+      onRefresh();
+      setTimeout(() => {
+        setRotating(false);
+      }, 1000);
+    }
+  };
+
+  return (
+    <button
+      className={`refresh-button ${rotating ? "rotating" : ""}`}
+      onClick={handleRefresh}
+    >
+      <FiRefreshCw className="refresh-button-icon" />
+    </button>
+  );
+};
+
+export default RefreshButton;
